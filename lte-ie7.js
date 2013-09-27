a8308b361b818eb13739a8af98cd18a31b6cf7ee
Hanzel mata <hachewd@gmail.com>
2013-09-27 10:20:38 -0600

    primer release
    opte por subir porque reemplace el archivo index.html por otro archivo
    index. empezar de nuevo :-(

---
+++ lte-ie7.js
@@ -0,0 +1,38 @@
+/* Load this script using conditional IE comments if you need to support IE 7 and IE 6. */
+
+window.onload = function() {
+	function addIcon(el, entity) {
+		var html = el.innerHTML;
+		el.innerHTML = '<span style="font-family: \'icomoon\'">' + entity + '</span>' + html;
+	}
+	var icons = {
+			'icon-x-altx-alt' : '&#xe002;',
+			'icon-move-alt2' : '&#xe000;',
+			'icon-facebook' : '&#xe003;',
+			'icon-twitter' : '&#xe001;',
+			'icon-home' : '&#xe004;',
+			'icon-phone' : '&#xe005;',
+			'icon-envelope' : '&#xe006;',
+			'icon-share' : '&#xe007;',
+			'icon-library' : '&#xe008;',
+			'icon-profile' : '&#xe009;',
+			'icon-user' : '&#xe00a;'
+		},
+		els = document.getElementsByTagName('*'),
+		i, attr, c, el;
+	for (i = 0; ; i += 1) {
+		el = els[i];
+		if(!el) {
+			break;
+		}
+		attr = el.getAttribute('data-icon');
+		if (attr) {
+			addIcon(el, attr);
+		}
+		c = el.className;
+		c = c.match(/icon-[^\s'"]+/);
+		if (c && icons[c[0]]) {
+			addIcon(el, icons[c[0]]);
+		}
+	}
+};
